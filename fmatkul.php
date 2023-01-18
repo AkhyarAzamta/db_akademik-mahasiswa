ba8df415005932e0941c55d0f3ef7c8791dbc680
Akhyar.Azamta <bebyraska14@gmail.com>
2023-01-18 17:18:36 +0700

    menampilkan matkul & dosen

---
+++ fmatkul.php
@@ -0,0 +1,60 @@
+<?php
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+if($op == 'delete'){
+    $id         = $_GET['KodeMK'];
+    $sql1       = "delete from Matakuliah where KodeMK = '$id'";
+    $q1         = mysqli_query($koneksi,$sql1);
+    if($q1){
+        $sukses = "Berhasil hapus data";
+    }else{
+        $error  = "Gagal melakukan delete data";
+    }
+}
+if ($op == 'edit') {
+    $id         = $_GET['KodeMK'];
+    $sql1       = "select * from Matakuliah where KodeMK = '$id'";
+    $q1         = mysqli_query($koneksi, $sql1);
+    $r1         = mysqli_fetch_array($q1);
+    $KodeMK        = $r1['KodeMK'];
+    $NamaMatakuliah       = $r1['NamaMatakuliah'];
+    $SKS  = $r1['SKS'];
+    $IDProdi  = $r1['IDProdi'];
+
+    if ($KodeMK == '') {
+        $error = "Data tidak ditemukan";
+    }
+}
+if (isset($_POST['simpan'])) { //untuk create
+    $KodeMK        = $_POST['KodeMK'];
+    $NamaMatakuliah       = $_POST['NamaMatakuliah'];
+    $SKS  = $_POST['SKS'];
+    $IDProdi  = $_POST['IDProdi'];
+
+
+    if ($KodeMK && $NamaMatakuliah && $SKS && $IDProdi) {
+        if ($op == 'edit') { //untuk update
+            $sql1       = "update Matakuliah set KodeMK = '$KodeMK',NamaMatakuliah='$NamaMatakuliah',SKS='$SKS',IDProdi='$IDProdi' where KodeMK = '$id'";
+            $q1         = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Data berhasil diupdate";
+            } else {
+                $error  = "Data gagal diupdate";
+            }
+        } else { //untuk insert
+            $sql1   = "insert into Matakuliah values ('$KodeMK','$NamaMatakuliah','$SKS','$IDProdi')";
+            $q1     = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses     = "Berhasil memasukkan data baru";
+            } else {
+                $error      = "Gagal memasukkan data";
+            }
+        }
+    } else {
+        $error = "Silakan masukkan semua data";
+    }
+}
+?>
