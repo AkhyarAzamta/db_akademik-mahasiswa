937c7d057f9b819126c58ab2f6782e7ee092c5e2
Akhyar.Azamta <bebyraska14@gmail.com>
2023-01-19 17:00:15 +0700

    eror dosen dan matkul

--- add.php
+++ add.php
@@ -1,6 +1,6 @@
 <?php
-require 'koneksi.php';
-require 'functions.php';
+require './config/koneksi.php';
+require './config/functions.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,10 +10,34 @@ require 'functions.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Tambah Data</title>
 </head>
 
-<body>
+<body><nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Akademik</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./dosen/dosen.php">Dosen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="./matkul/matkul.php">Matkul</a>
+                    </li>
+                </ul>
+                <form action="./index.php" method="post" class="d-flex">
+                    <input class="form-control me-2" name="NIM" type="search" placeholder="Cari Data Mahasiswa" aria-label="Search">
+                    <button class="btn btn-outline-success" name="search" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
     <div class="mx-auto m-5">
         <!-- untuk memasukkan data -->
         <div class="card">
